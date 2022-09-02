b92427899bc52d354972a79fb9d3606210ca70ba
ANSHU KUMARI <anshukumari@ANSHUs-MacBook-Pro.local>
2022-09-02 14:21:51 +0530

    Initial commit | Anshu

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# I am going to build todoapp
+
+As a user, I can
+
+read a list of tasks.
+add a task using the mouse or keyboard.
+mark any task as completed, using the mouse or keyboard.
+delete any task, using the mouse or keyboard.
+edit any task, using the mouse or keyboard.
+view a specific subset of tasks: All tasks, only the active task, or only the completed tasks.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App tasks={DATA} />
+   </React.StrictMode>
+ );
+
 reportWebVitals();
